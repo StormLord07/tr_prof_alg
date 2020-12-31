6299371ff9a39c02e6b740588f6d7a56d51e4511
StormLord07 <50772416+StormLord07@users.noreply.github.com>
2020-12-31 21:48:22 +0300

    Add files via upload

---
+++ num1.jl
@@ -0,0 +1,17 @@
+include("robot_library.jl")
+
+
+function cross_marks!(r)
+    side = Up
+    for i in 0:1:3
+        steps = 0
+        while isborder(r,side)==false
+            move!(r,side)
+            putmarker!(r)
+            steps += 1
+        end
+        movements!(r, inverse(side), steps)
+        side = left(side)
+    end
+    putmarker!(r)
+end
